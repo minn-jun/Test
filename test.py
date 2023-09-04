455b1d14a3d2ad82f0c7afb904d814cc59b724f6
Minjun Ko <kohmj0324@naver.com>
2023-09-04 19:35:21 +0900

    third commit

--- test.py
+++ test.py
@@ -6,3 +6,22 @@ for i in range(1, 10):
 
 
 # github experinece ++
+
+# git status
+
+# git add .   # 전체를 업로드
+# or
+# git add file/temp.py  # 원하는 파일만 업로드
+
+# git commit -m "first commit"
+
+# git push origin main
+
+# ----------------------------
+
+# git log   # commit 로그 확인
+
+# git reset --hard HEAD^  # 가장 최근 commit 로그 삭제
+# git reset --hard HEAD~n  # n 만큼 최근 commit 로그 삭제
+
+# git push -f origin master  # -f 를 이용해서 혹시 모를 충돌방지. 강제 업데이트
